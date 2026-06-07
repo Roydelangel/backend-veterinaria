0a6727df4e431e1fee6d4619525492e4f6cede26
Roydelangel <roydelangelaguero@gmail.com>
2026-06-06 22:42:57 -0400

    actualizarPaciente

--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import { agregarPaciente, obtenerPacientes } from '../controllers/pacienteController.js';
+import { agregarPaciente, obtenerPacientes,  obtenerPaciente, actualizarPaciente, eliminarPaciente } from '../controllers/pacienteController.js';
 import checkAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/')
-.post(checkAuth, agregarPaciente)
-.get(checkAuth, obtenerPacientes);
+    .post(checkAuth, agregarPaciente)
+    .get(checkAuth, obtenerPacientes);
+
+router.route('/:id')
+    .get(checkAuth, obtenerPaciente)
+    .put(checkAuth, actualizarPaciente)
+    .delete(checkAuth, eliminarPaciente);
 
 
 export default router; 
