dff0e914c7db6957441704f9f2b93fe4d1ee0e17
Roydelangel <roydelangelaguero@gmail.com>
2026-06-06 22:20:07 -0400

    obtener pacientes

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -1,18 +1,22 @@
 import Paciente from "../models/Paciente.js";
 
 
-const agregarPaciente = (req, res) => {
+const agregarPaciente = async (req, res) => {
     const paciente = new Paciente(req.body);
+    paciente.veterinario = req.veterinario._id;
 
     try {
-          
+        const pacienteGuardado = await paciente.save();
+        return res.json(pacienteGuardado);
     } catch (error) {
         console.log(error);
     };
 };
 
-const obtenerPacientes = (req, res) => {
+const obtenerPacientes = async (req, res) => {
+    const pacientes = await Paciente.find().where('veterinarios').equals(req.veterinario);
 
+    res.json(pacientes);
 };
 
 export {
--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { agregarPaciente, obtenerPacientes } from '../controllers/pacienteController.js';
+import checkAuth from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(agregarPaciente).get(obtenerPacientes)
+router.route('/')
+.post(checkAuth, agregarPaciente)
+.get(checkAuth, obtenerPacientes);
 
 
 export default router; 
